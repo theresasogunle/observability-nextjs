70cafbc67c5ebee81f828dc8a559ebb683547e07
theresasogunle <sogunledolapo@gmail.com>
2025-08-03 21:59:20 +0100

    test deployment

--- app/products/page.js
+++ app/products/page.js
@@ -1,6 +1,13 @@
 // app/products/page.js
 async function getProducts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products`);
+
+    // Use absolute URL in production, relative in development
+  const baseUrl = process.env.VERCEL_URL 
+    ? `https://${process.env.VERCEL_URL}` 
+    : process.env.NODE_ENV === 'development' 
+    ? 'http://localhost:3000'
+    : '';
+  const res = await fetch(`${baseUrl}/api/products`);
   if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
 }
